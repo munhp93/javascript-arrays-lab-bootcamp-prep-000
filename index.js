d512167a055a71632df1c5e30171d06a2e74e91d
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-05 01:35:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = "I don't do much."
 var kittens=['Milo','Otis','Garfield'];
 console.log(kittens);
 function destructivelyAppendKitten(name){
-  appendKitten();
+  appendKitten("Ralph");
 }
